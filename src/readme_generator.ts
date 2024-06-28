f259effc720f0341adf66c884cfb39268cdb8a3a
Gakuto1112 <gakuto131112@gmail.com>
2024-06-28 22:41:39 +0900

    引数チェック機能を削除

--- src/readme_generator.ts
+++ src/readme_generator.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import readline from "readline";
-import { error, warn } from "./logger";
+import { warn } from "./logger";
 
 class ReadmeGenerator {
     /**
@@ -27,7 +27,7 @@ class ReadmeGenerator {
      * @param inputPath 入力するテンプレートのパス
      * @param outputPath 生成するREADMEの出力先のパス
      */
-    public async generateReadme(inputPath: string, outputPath: string): Promise<void> {
+    private async generateReadme(inputPath: string, outputPath: string): Promise<void> {
         const writeStream: fs.WriteStream = fs.createWriteStream(outputPath, {encoding: "utf-8"});
         for await (let line of readline.createInterface({input: fs.createReadStream(inputPath, {encoding: "utf-8"}), output: writeStream})) {
             //画像のソースファイルの置き換え
@@ -64,54 +64,18 @@ class ReadmeGenerator {
             writeStream.write(`${line.substring(charCount)}\n`);
         }
     }
-}
-
-/**
- * メイン関数
- */
-async function main(): Promise<void> {
-    let errorFound: boolean = false;
 
     /**
-     * コマンドライン引数を確認する。
-     * @param argIndex 確認する引数のインデクス番号（1つ目の引数は0、2つ目の引数は1、...）
-     * @param errorMessage 引数が指定されていない場合のエラーメッセージ
-     * @returns 引数が指定されていればtrue、指定されていなければfalseを返す。
+     * メイン関数
      */
-    function checkArgs(argIndex: number, argName: string): boolean {
-        if(process.argv[argIndex + 2] != undefined && process.argv[argIndex + 2].length > 0) return true;
-        else {
-            error(`Argument ${argIndex + 1} was expected to be the ${argName}, but not specified.`);
-            errorFound = true;
-            return false;
-        }
-    }
-
-    //引数の確認
-    //0. READMEのルートディレクトリ
-    if(checkArgs(0, "readme root path")) {
-        if(!fs.existsSync(process.argv[2])) {
-            error(`"${process.argv[2]}" was specified as the readme root path, but it doesn't exist.`);
-            errorFound = true;
-        }
-    }
-
-    //1. READMEがあるレポジトリ名
-    if(checkArgs(1, "repository name")) {
-        if(!/^[a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9])\/[\w\-\.]+$/i.test(process.argv[3])) {
-            error(`"${process.argv[3]}" was specified as the repository name, but it is invalid.`);
-            errorFound = true;
-        }
-    }
-
-    if(!errorFound) {
-        const readmeGenerator: ReadmeGenerator = new ReadmeGenerator(process.argv[3]);
+    public async main(): Promise<void> {
         console.info("Generating README.md...");
-        await readmeGenerator.generateReadme(`${process.argv[2]}/README_templates/en.md`, `${process.argv[2]}/README.md`);
+        await this.generateReadme(`${process.argv[2]}/README_templates/en.md`, `${process.argv[2]}/README.md`);
         console.info("Generating README_jp.md...");
-        await readmeGenerator.generateReadme(`${process.argv[2]}/README_templates/jp.md`, `${process.argv[2]}/README_jp.md`);
+        await this.generateReadme(`${process.argv[2]}/README_templates/jp.md`, `${process.argv[2]}/README_jp.md`);
     }
-    else process.exit(1);
 }
 
-main();
+if(require.main === module) {
+    new ReadmeGenerator(process.argv[3]).main();
+}
